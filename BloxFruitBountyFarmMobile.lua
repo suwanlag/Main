7bdd19750dd6ac1764482bdee20a19bf28361297
suwanlag <92519563+suwanlag@users.noreply.github.com>
2021-11-29 14:49:15 +0700

    Update BloxFruitBountyFarmMobile.lua

--- BloxFruitBountyFarmMobile.lua
+++ BloxFruitBountyFarmMobile.lua
@@ -1,10 +1,1 @@
-local key = _G.Key
-local check = "https://whitelistseenhubv2.000webhostapp.com/MainCheck.php?key=" .. key
-if game:HttpGet(check) == "Whitelisted" then
-loadstring(game:HttpGet("https://whitelistseenhubv2.000webhostapp.com/MainBounty"))()
-else
-game:GetService("StarterGui"):SetCore("SendNotification", {
-  Title = "Key",
-  Text = "InvalidKey!"
-})
-end
+print("RickRoll")
